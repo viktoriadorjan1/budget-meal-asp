3220e23a9d22cf53c674048eb9e6d2f4b158ea80
Viktoria Adorjan <va520@ic.ac.uk>
2023-11-07 13:24:18 +0000

    created function hello

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from test import test
-
 
 app = Flask(__name__)
 
 
+def hello():
+    return "Hello world"
+
+
 @app.route('/')
 def hello_world():
-    test()
+    hello()
