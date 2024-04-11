6e0c8bf49d08625642e6916d7bff0e77f7032737
Viktoria Adorjan <va520@ic.ac.uk>
2024-04-11 16:28:25 +0100

    updated mongoclient uri.

--- webstores.py
+++ webstores.py
@@ -5,7 +5,7 @@ from pymongo.server_api import ServerApi
 def get_relevant_webstore_data(ingredients):
     # connect to the server with new client
     uri = "mongodb+srv://admin:JKEw0feoZCxOE0LS@cluster0.m5iuzzq.mongodb.net/?retryWrites=true&w=majority" \
-          "&appName=Cluster0"
+          "&appName=Cluster0&ssl=true"
     client = MongoClient(uri, server_api=ServerApi('1'))
 
     db = client["webstores"]
