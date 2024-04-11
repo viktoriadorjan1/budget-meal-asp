640dd0e45f12c58c268dabeb1607938932034787
Viktoria Adorjan <va520@ic.ac.uk>
2024-04-11 16:34:27 +0100

    added certification.

--- webstores.py
+++ webstores.py
@@ -1,12 +1,15 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 
 
 def get_relevant_webstore_data(ingredients):
+    ca = certifi.where()
+
     # connect to the server with new client
     uri = "mongodb+srv://admin:JKEw0feoZCxOE0LS@cluster0.m5iuzzq.mongodb.net/?retryWrites=true&w=majority" \
-          "&appName=Cluster0&ssl=true"
-    client = MongoClient(uri, server_api=ServerApi('1'))
+          "&appName=Cluster0"
+    client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=ca)
 
     db = client["webstores"]
     collection = db["webstoreItems"]
